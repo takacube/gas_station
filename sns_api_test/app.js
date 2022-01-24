8bb17cc5e9efab1218471c4bf1e855f3a4824fb3
taka <shimakuratakanao@gmail.com>
2022-01-24 20:16:09 +0900

    set environment variable

--- sns_api_test/app.js
+++ sns_api_test/app.js
@@ -1,14 +1,17 @@
 function myFunction() {
-    const twitter_follower = count_follower_of_twitter();
-    Logger.log(twitter_follower);
+    const twitterfollower_count = count_follower_of_twitter();
+    Logger.log(twitterfollower_count);
 }
 
 function count_follower_of_twitter() {
-    const url = "https://api.twitter.com/2/users/by/username/taka55143344?user.fields=public_metrics";
+    const prop = PropertiesService.getScriptProperties().getProperties();
+    const USER_NAME = prop.USER;
+    const TOKEN = prop.TOKEN;
+    const url = "https://api.twitter.com/2/users/by/username/" + USER_NAME  + "?user.fields=public_metrics"; 
     const options = {
         "method": "get",
         "headers": {
-        "authorization": "Bearer [ここに自身のBearer tokenを入れる]"
+        "authorization": "Bearer " + TOKEN
         },
     };
     const response = JSON.parse(UrlFetchApp.fetch(url, options));
